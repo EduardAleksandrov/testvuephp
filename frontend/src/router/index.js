8be9885b7ac737289882b78aadbf1bbf54cd7caa
Eduard Aleksandrov <eduard.ale@list.ru>
2022-08-21 20:47:23 +0300

    some fixes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,20 +5,7 @@ const routes = [
     path: '/',
     name: 'home',
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
-  },
-  {
-    path: '/one',
-    name: 'one',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/One.vue')
-  },
-  {
-    path: '/form',
-    name: 'form',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Form.vue')
-  },
+  }
 ]
 
 const router = createRouter({
